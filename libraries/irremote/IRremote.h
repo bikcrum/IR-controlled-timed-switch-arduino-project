1b1926d860d24096da413f06a8d15d729107ff7d
Bikram Pandit <bikram@proshore.eu>
2018-10-27 23:36:45 +0545

    add libraries

---
+++ libraries/irremote/IRremote.h
@@ -0,0 +1,74 @@
+#ifndef IRremote_h
+#define IRremote_h
+#include "IRremoteInt.h"
+
+#define PRONTO_ONCE        false
+#define PRONTO_REPEAT      true
+#define PRONTO_FALLBACK    true
+#define PRONTO_NOFALLBACK  false
+
+typedef
+	enum {
+		UNKNOWN      = -1,
+		UNUSED       =  0
+	}
+decode_type_t;
+
+#define DEBUG  0
+
+#if DEBUG
+#	define DBG_PRINT(...)    Serial.print(__VA_ARGS__)
+#	define DBG_PRINTLN(...)  Serial.println(__VA_ARGS__)
+#else
+#	define DBG_PRINT(...)
+#	define DBG_PRINTLN(...)
+#endif
+
+int  MATCH       (int measured, int desired) ;
+int  MATCH_MARK  (int measured_ticks, int desired_us) ;
+int  MATCH_SPACE (int measured_ticks, int desired_us) ;
+
+class decode_results
+{
+	public:
+		decode_type_t          decode_type;  // UNKNOWN, NEC, SONY, RC5, ...
+		unsigned int           address;      // Used by Panasonic & Sharp [16-bits]
+		unsigned long          value;        // Decoded value [max 32-bits]
+		int                    bits;         // Number of bits in decoded value
+		volatile unsigned int  *rawbuf;      // Raw intervals in 50uS ticks
+		int                    rawlen;       // Number of records in rawbuf
+		int                    overflow;     // true iff IR raw code too long
+};
+
+#define REPEAT 0xFFFFFFFF
+
+class IRrecv
+{
+	public:
+		IRrecv (int recvpin) ;
+		IRrecv (int recvpin, int blinkpin);
+
+		void  blink13    (int blinkflag) ;
+		int   decode     (decode_results *results) ;
+		void  enableIRIn ( ) ;
+		bool  isIdle     ( ) ;
+		void  resume     ( ) ;
+
+	private:
+		long  decodeHash (decode_results *results) ;
+		int   compare    (unsigned int oldval, unsigned int newval) ;
+} ;
+
+class IRsend
+{
+	public:
+		IRsend () { }
+
+		void  custom_delay_usec (unsigned long uSecs);
+		void  enableIROut 		(int khz) ;
+		void  mark        		(unsigned int usec) ;
+		void  space       		(unsigned int usec) ;
+		void  sendRaw     		(const unsigned int buf[],  unsigned int len,  unsigned int hz) ;
+} ;
+
+#endif
