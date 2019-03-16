6ec03df370638a7756a1d0c083782b6e58089eb8
Bikram Pandit <bikram@proshore.eu>
2019-03-16 13:49:49 +0545

    update code

--- irproject.ino
+++ irproject.ino
@@ -10,28 +10,29 @@
 #define TIME_GAP 5000
 
 long remote_number_buttons[10] = {
-  3238126971,4039382595,2538093563,2259740311,2327013275, 2666828831, 2747854299,1541889663,4287727287,4084712887};
+  3238126971, 4039382595, 2538093563, 2259740311, 2327013275, 2666828831, 2747854299, 1541889663, 4287727287, 4084712887
+};
 
-int RECV_PIN = 3;
-IRrecv irrecv(RECV_PIN);
+int RECEIVER_PIN = 3;
+IRrecv irrecv(RECEIVER_PIN);
 decode_results results;
 
 int RELAY_PIN = 5;
 
 /* pin map of seven segments
- 11
- ---
- 13 |   | 9
- | 7 |
- ---
- 2 |   | 6
- |   |
- ---
- 4
+_______11
+_______---
+___13 |   | 9
+______| 7 |
+_______---
+____2 |   | 6
+______|   |
+_______---
+________4
  */
 
 #include "SevSeg.h"
-SevSeg sevseg; 
+SevSeg sevseg;
 SimpleTimer timer;
 
 int remaining_time = 999;
@@ -40,16 +41,18 @@ void setup()
 {
   setupSevSeg();
   setupIRRemote();
-  pinMode(RELAY_PIN, OUTPUT); 
+  pinMode(RELAY_PIN, OUTPUT);
   timeAvailable();
 }
 
-void setupSevSeg(){
+void setupSevSeg() {
   byte numDigits = 2;
   byte digitPins[] = {
-    10,8                };
+    10, 8
+  };
   byte segmentPins[] = {
-    11, 9, 6, 4, 2, 13, 7                };
+    11, 9, 6, 4, 2, 13, 7
+  };
   bool resistorsOnSegments = true; // 'false' means resistors are on digit pins
   byte hardwareConfig = COMMON_ANODE; // See README.md for options
   bool updateWithDelays = false; // Default. Recommended
@@ -59,9 +62,9 @@ void setupSevSeg(){
   sevseg.setBrightness(100);
 }
 
-void setupIRRemote(){
+void setupIRRemote() {
   ///Serial.begin(9600);
-  irrecv.enableIRIn(); 
+  irrecv.enableIRIn();
 }
 
 boolean waiting_for_next_input_digit = false;
@@ -75,108 +78,108 @@ void loop() {
   }
 
 
-  sevseg.setNumber(remaining_time,1);
+  sevseg.setNumber(remaining_time, 1);
 
   sevseg.refreshDisplay();
   timer.run();
 
-  if(waiting_for_next_input_digit && millis() >= waiting_for_next_input_digit_expiry_time){
+  if (waiting_for_next_input_digit && millis() >= waiting_for_next_input_digit_expiry_time) {
     waiting_for_next_input_digit = false;
   }
 }
 
 int timer_id = 0;
 
-void signalReceived(unsigned long value){
-  switch(value){
-  case REMOTE_UP_BUTTON:
-    remaining_time = (remaining_time + 1)%100;
-    updateTimer();
-    break;
-  case REMOTE_DOWN_BUTTON:
-    if(remaining_time == 0){
-      remaining_time = 99;
-    }
-    else{
-      remaining_time--;
-    }
-    updateTimer();
-    break;
-  case REMOTE_LEFT_BUTTON:
-    remaining_time = remaining_time - 10;
-    if(remaining_time < 0){
-      remaining_time += 100;
-    }
-  
-    updateTimer();
-    break;
-  case REMOTE_RIGHT_BUTTON:
-    remaining_time = (remaining_time + 10)%100;
-    updateTimer();
-    break;
-  case REMOTE_POWER_BUTTON:
-    deleteTimer();
-    waiting_for_next_input_digit = false;
-    remaining_time = remaining_time == 0?999:0;
-    if(remaining_time == 0){
-      timeExpired();
-    }
-    else{
-      timeAvailable();
-    }
-    break;
-  default:
-    for(int i =0;i<10;i++){
-      if(value == remote_number_buttons[i]){
-        if(waiting_for_next_input_digit){
-          remaining_time = remaining_time * 10 + i;
-          waiting_for_next_input_digit = false;
-        }
-        else{
-          remaining_time = i;
-          waiting_for_next_input_digit = true;
-          waiting_for_next_input_digit_expiry_time = millis() + INPUT_DIGIT_DELAY;
-        }
-        updateTimer();
-        break;
+void signalReceived(unsigned long value) {
+  switch (value) {
+    case REMOTE_UP_BUTTON:
+      remaining_time = (remaining_time + 1) % 100;
+      updateTimer();
+      break;
+    case REMOTE_DOWN_BUTTON:
+      if (remaining_time == 0) {
+        remaining_time = 99;
       }
-    }
+      else {
+        remaining_time--;
+      }
+      updateTimer();
+      break;
+    case REMOTE_LEFT_BUTTON:
+      remaining_time = remaining_time - 10;
+      if (remaining_time < 0) {
+        remaining_time += 100;
+      }
+
+      updateTimer();
+      break;
+    case REMOTE_RIGHT_BUTTON:
+      remaining_time = (remaining_time + 10) % 100;
+      updateTimer();
+      break;
+    case REMOTE_POWER_BUTTON:
+      deleteTimer();
+      waiting_for_next_input_digit = false;
+      remaining_time = remaining_time == 0 ? 999 : 0;
+      if (remaining_time == 0) {
+        timeExpired();
+      }
+      else {
+        timeAvailable();
+      }
+      break;
+    default:
+      for (int i = 0; i < 10; i++) {
+        if (value == remote_number_buttons[i]) {
+          if (waiting_for_next_input_digit) {
+            remaining_time = remaining_time * 10 + i;
+            waiting_for_next_input_digit = false;
+          }
+          else {
+            remaining_time = i;
+            waiting_for_next_input_digit = true;
+            waiting_for_next_input_digit_expiry_time = millis() + INPUT_DIGIT_DELAY;
+          }
+          updateTimer();
+          break;
+        }
+      }
   }
 }
 
-void deleteTimer(){
+void deleteTimer() {
   timer.deleteTimer(timer_id);
 }
-void updateTimer(){
+void updateTimer() {
   deleteTimer();
   remainingTimeCheck();
-  timer_id = timer.setInterval(TIME_GAP,countdown);
+  timer_id = timer.setInterval(TIME_GAP, countdown);
 }
 
-void timeExpired(){
+void timeExpired() {
   //do something
   //Serial.println("time expired");
   digitalWrite(RELAY_PIN, LOW);
 }
 
-void timeAvailable(){
+void timeAvailable() {
   //do something
   //Serial.println("time available");
   digitalWrite(RELAY_PIN, HIGH);
 }
 
-void countdown(){
+void countdown() {
   remaining_time --;
   remainingTimeCheck();
 }
 
-void remainingTimeCheck(){
-  if(remaining_time <= 0){
+void remainingTimeCheck() {
+  if (remaining_time <= 0) {
     remaining_time = 0;
     timer.deleteTimer(timer_id);
     timeExpired();
   }
-  else{
+  else {
     timeAvailable();
   }
 }
